e8f796ef2d7a3d334fb4e49e9272ace9d51caef1
Marina Fuster <37815201+MarinaFuster@users.noreply.github.com>
2024-05-23 17:07:25 -0300

    :art: Transformers implementation with tutorial (#1)

---
+++ transformers/tutorial/attention.py
@@ -0,0 +1,89 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Head(nn.Module):
+    """ one head of self-attention """
+
+    def __init__(self, head_size, n_embd, block_size, dropout):
+        """
+        This class is a single head of self-attention. 
+        It takes as input a tensor of size (batch, time-step, channels) 
+        and returns a tensor of size (batch, time-step, head size) where 
+        the head size is the size of the output of the attention mechanism.
+
+        Args:
+            head_size (_type_): _description_
+            n_embd (_type_): _description_
+            block_size (_type_): _description_
+            dropout (_type_): _description_
+        """
+        super().__init__()
+        self.key = nn.Linear(n_embd, head_size, bias=False) # bias is usually off
+        self.query = nn.Linear(n_embd, head_size, bias=False)
+        self.value = nn.Linear(n_embd, head_size, bias=False)
+        
+        # this is not a parameter and convention is to call it buffer
+        # it's the matrix we are using to get our lower diagonal for attention
+        self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+
+        # not used at this point
+        # self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        """
+        This function represents the forward pass of the self-attention mechanism.
+
+        Args:
+            x: this is the input of dimensions (B,T,C) where B is the batch size, 
+            T is the time-step and C is the number of channels, which in this case
+            is n_embed, the number of embeddings after generating an encoding
+            that contains information about the characters/words in the input and
+            the position of the characters/words in the input.
+
+        Returns:
+            tensor: (B, T, head_size) where B is the batch size, T is the time-step
+            and head_size is the size of the output of the attention mechanism.
+        """
+        # input of size (batch, time-step, channels)
+        # output of size (batch, time-step, head size)
+        B, T, C = x.shape
+        k = self.key(x)   # (B,T,hs)
+        q = self.query(x) # (B,T,hs)
+        # compute attention scores ("affinities")
+        wei = q @ k.transpose(-2,-1) * k.shape[-1]**-0.5 # (B, T, hs) @ (B, hs, T) -> (B, T, T)
+        wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf')) # (B, T, T), decoder block
+        wei = F.softmax(wei, dim=-1) # (B, T, T)
+        
+        # not used at this point
+        #wei = self.dropout(wei)
+        
+        # perform the weighted aggregation of the values
+        v = self.value(x) # (B,T,hs)
+        out = wei @ v # (B, T, T) @ (B, T, hs) -> (B, T, hs)
+        return out
+
+
+class MultiHeadAttention(nn.Module):
+    """ multiple heads of self-attention in parallel """
+
+    def __init__(self, num_heads, head_size, n_embd, block_size, dropout):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(
+            head_size=head_size, 
+            n_embd=n_embd, 
+            block_size=block_size, 
+            dropout=dropout
+        ) for _ in range(num_heads)])
+        
+        # self.proj = nn.Linear(head_size * num_heads, n_embd)
+        # self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        # this operation is simular to group convolution
+        out = torch.cat([h(x) for h in self.heads], dim=-1)
+        
+        #out = self.dropout(self.proj(out))
+        
+        return out
---
+++ transformers/tutorial/bigram_with_attention.py
@@ -0,0 +1,160 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+from attention import MultiHeadAttention
+
+# hyperparameters
+batch_size = 32 # how many independent sequences will we process in parallel?
+block_size = 8 # what is the maximum context length for predictions?
+max_iters = 3000
+eval_interval = 300
+learning_rate = 1e-3 # attention mechanisms do not handle large learning rates well
+device = 'cpu'
+eval_iters = 200
+n_embd = 32 # TODO: this still requires formal definition. The context can't be bigger than this.
+# ------------
+
+torch.manual_seed(1337)
+
+with open('input.txt', 'r', encoding='utf-8') as f:
+    text = f.read()
+
+# here are all the unique characters that occur in this text
+chars = sorted(list(set(text)))
+vocab_size = len(chars)
+# create a mapping from characters to integers
+stoi = { ch:i for i,ch in enumerate(chars) }
+itos = { i:ch for i,ch in enumerate(chars) }
+encode = lambda s: [stoi[c] for c in s] # encoder: take a string, output a list of integers
+decode = lambda l: ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
+
+# Train and test splits
+encoded_dataset = torch.tensor(encode(text), dtype=torch.long)
+n = int(0.9*len(encoded_dataset)) # first 90% will be train, rest val
+train_data = encoded_dataset[:n]
+val_data = encoded_dataset[n:]
+
+# data loading
+def get_batch(split):
+    # generate a small batch of data of inputs x and targets y
+    data = train_data if split == 'train' else val_data
+    ix = torch.randint(len(data) - block_size, (batch_size,))
+    x = torch.stack([data[i:i+block_size] for i in ix])
+    y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+    x, y = x.to(device), y.to(device)
+    return x, y
+
+# this tells torch that no backward pass is going to be called
+# which means that it can be more efficient about computation/memory
+# because it does not have to store all intermediate variables to compute
+# backward propagation
+@torch.no_grad()
+def estimate_loss():
+    out = {}
+    # indicating whether the model is in evaluation or training mode 
+    # doesn't have any effect in this model, but it's good practice
+    # because layers like dropout behave differently at inference with
+    # respect to training
+    model.eval()
+    for split in ['train', 'val']:
+        losses = torch.zeros(eval_iters)
+        for k in range(eval_iters):
+            X, Y = get_batch(split)
+            _, loss = model(X, Y)
+            losses[k] = loss.item()
+        out[split] = losses.mean() # we average across different batches
+    model.train()
+    return out
+
+# super simple bigram model
+class BigramLanguageModel(nn.Module):
+
+    def __init__(self, vocab_size):
+        super().__init__()
+        self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
+        self.position_embedding_table = nn.Embedding(block_size, n_embd)
+        
+        # attention mechanism
+        # self.self_attention_head = Head(head_size=n_embd, n_embd=n_embd, block_size=block_size, dropout=0.2)
+        
+        # now, we have multiple communication channels (shorter) which through the tokens can communicate
+        self.self_attention_heads = MultiHeadAttention(
+            num_heads=4, 
+            head_size=n_embd//4, # 4 heads of 8-dimensional self-attention
+            n_embd=n_embd, 
+            block_size=block_size, 
+            dropout=0.2
+        )
+        
+        self.language_model_head = nn.Linear(n_embd, vocab_size)
+
+    def forward(self, idx, targets=None):
+        B, T = idx.shape
+
+        tok_embd = self.token_embedding_table(idx) # (B, T, n_embd)
+        print(tok_embd.shape)
+        pos_embd = self.position_embedding_table(torch.arange(block_size, device=device)) # (T, n_embd)
+        print(pos_embd.shape)
+        x = tok_embd + pos_embd # (B, T, n_embd)
+        x = self.self_attention_heads(x) # (B, T, head_size)
+        
+        # if we jump from attention to calculating the logics, we can think about it
+        # as if the tokens have communicated between each other, they've gather data
+        # but they don't have a lot of time to "reflect" or "think" about what
+        # they've learned and how to use it to calculate the logits.
+        logits = self.language_model_head(x) # (B, T, vocab_size)
+
+        if targets is None:
+            loss = None
+        else:
+            B, T, C = logits.shape
+            logits = logits.view(B*T, C)
+            targets = targets.view(B*T)
+            loss = F.cross_entropy(logits, targets)
+
+        return logits, loss
+
+    def generate(self, idx, max_new_tokens):
+        # idx is (B, T) array of indices in the current context
+        for _ in range(max_new_tokens):
+            print(idx.shape)
+            # crop the context because it cant be larger than block_size due to positional embedding table
+            idx_cropped = idx[:, -block_size:] 
+            # get the predictions
+            logits, _ = self(idx_cropped)
+            # focus only on the last time step
+            logits = logits[:, -1, :] # becomes (B, C)
+            # apply softmax to get probabilities
+            probs = F.softmax(logits, dim=-1) # (B, C)
+            # sample from the distribution
+            idx_next = torch.multinomial(probs, num_samples=1) # (B, 1)
+            # append sampled index to the running sequence
+            idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
+        return idx
+
+model = BigramLanguageModel(vocab_size)
+m = model.to(device)
+
+# create a PyTorch optimizer
+optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
+
+for iter in range(max_iters):
+
+    # every once in a while evaluate the loss on train and val sets
+    if iter % eval_interval == 0:
+        losses = estimate_loss()
+        print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
+
+    # sample a batch of data
+    xb, yb = get_batch('train')
+
+    # evaluate the loss
+    logits, loss = model(xb, yb)
+    optimizer.zero_grad(set_to_none=True)
+    loss.backward()
+    optimizer.step()
+
+# generate from the model
+# context = torch.zeros((1, 1), dtype=torch.long, device=device)
+# print(decode(m.generate(context, max_new_tokens=500)[0].tolist()))
